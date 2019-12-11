ddfa89a6efaa023b912ce48d09e9d4a6206ee8ef
Akshay Hiregoudar <51836938+akshayhiregoudar@users.noreply.github.com>
2019-12-11 13:01:39 -0600

    Update mavlink_control.cpp

--- mavlink_control.cpp
+++ mavlink_control.cpp
@@ -219,7 +219,8 @@ commands(Autopilot_Interface &api)
 	for (int i=0; i < 8; i++)
 	{
 		mavlink_local_position_ned_t pos = api.current_messages.local_position_ned;
-		mavlink_highres_imu_t imu = api.current_messages.highres_imu;
+		Mavlink_Messages messages = api.current_messages;
+		mavlink_highres_imu_t imu = messages.highres_imu;
 		printf("%i CURRENT POSITION XYZ = [ % .4f , % .4f , % .4f ] \n", i, pos.x, pos.y, pos.z);
 		printf("acc_x =  % f, acc_y = %f, acc_z = %f \n", i, imu.xacc, imu.yacc, imu.zacc); // (m/s^2)
 		printf("gyro_x = % f, gyro_y = %f, gyro_z = %f \n", i, imu.xgyro, imu.ygyro, imu.zgyro); // (rad/s)
